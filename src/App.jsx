12d4bdd24e1ad34712970ffa16864f6d80451cc3
Bojan <bojan-stanimirovski097@hotmail.com>
2023-12-15 16:29:48 +0100

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,57 @@
 import "./App.css";
 
-import React, { useState } from "react";
+import React, { Component } from "react";
 
-const App = () => {
-  const [monsters, setMonsters] = useState([
-    {
-      name: "Linda",
-      id: 1,
-    },
-    {
-      name: "Frank",
-      id: 2,
-    },
-    {
-      name: "Jackie",
-      id: 3,
-    },
-    {
-      name: "James",
-      id: 4,
-    },
-  ]);
+class App extends Component {
+  constructor() {
+    super();
 
-  return (
-    <div>
-      {monsters.map((monster) => (
-        <p key={monster.id}>{monster.name}</p>
-      ))}
-    </div>
-  );
-};
+    this.state = {
+      monsters: [
+        {
+          name: "Linda",
+          id: 1,
+        },
+        {
+          name: "Frank",
+          id: 2,
+        },
+        {
+          name: "Jackie",
+          id: 3,
+        },
+        {
+          name: "James",
+          id: 4,
+        },
+      ],
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) =>
+        this.setState(
+          () => {
+            return { monsters: users };
+          },
+          () => {
+            console.log(this.state);
+          }
+        )
+      );
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {this.state.monsters.map((monster) => (
+          <p key={monster.id}>{monster.name}</p>
+        ))}
+      </div>
+    );
+  }
+}
 
 export default App;
