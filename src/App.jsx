1febbb8d8bf2eca6d39e4869b425e03afb1cfe10
Bojan Stanimirovski <bojan-stanimirovski097@hotmail.com>
2024-01-08 19:01:55 +0100

    adding Title to App

--- src/App.jsx
+++ src/App.jsx
@@ -42,6 +42,7 @@ class App extends Component {
 
     return (
       <div className="App">
+        <h1 className="app-title">Monsters Rolodex</h1>
         <SearchBar
           onChangeHandler={onSearchChange}
           placeholder="Search Monsters"
